f39439e65d423e094f44adbe38d45fc313472335
ravi ranjan <ravi848101mnb@gmail.com>
2024-08-12 16:43:31 +0530

    chore: Update navbar title and styling

--- DisplayTopNproduct/src/components/NavBar/NavBar.jsx
+++ DisplayTopNproduct/src/components/NavBar/NavBar.jsx
@@ -5,10 +5,10 @@ const NavBar = () => {
     return (
         <div className=' '>
             <nav className='bg-red-800 h-20 p-10 text-white flex justify-between items-center'>
-                <h1 className='text-3xl font-bold'>my Ecommerce app</h1>
+                <h1 className='text-2xl font-bold'>myApp</h1>
                 <ul className='flex justify-between'>
                     <li className='p-2'>
-                        <NavLink to='/' activeClassName='text-red-500' className='hover:text-red-500 text-2xl' >Home</NavLink>
+                        <NavLink to='/' className='hover:text-red-500 text-2xl' >Home</NavLink>
                     </li>
                 </ul>
             </nav>
